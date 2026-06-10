ddea7f9ea2ed8e92bf98cfe71416168883c7ab1d
sabaka-chabaka <gamekimg360@gmail.com>
2026-06-10 14:18:29 +0400

    be

--- SabakaMarket.Domain/Entities/CartSession.cs
+++
@@ -1,9 +0,0 @@
-namespace SabakaMarket.Domain.Entities;
-
-public class CartSession
-{
-    public Guid Id { get; set; }
-    public Seller Seller { get; set; } = null!;
-    public ICollection<Product> Products { get; set; } = null!;
-    public decimal TotalPrice { get; set; }
-}
